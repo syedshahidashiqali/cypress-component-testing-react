6b7bd73d7d75995b1693fa76152643574ea5f018
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-10-31 18:02:48 +0500

    initilaize project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello from Shahid!</h1>
+    </div>
+  );
+}
+
+export default App;
